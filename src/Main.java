4864af34c6ef5274f0f47e3e6f39714c7103f2e5
Delgadoc86 <delgadoc1986@hotmail.com>
2023-07-12 12:48:43 -0300

    codigo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*  BUCLE FOR (PARA)
+
+   PARA (inicializacion; comparacion; acccion)
+   PARA (; contador mayor que 0 ; contador = contador - 1)*/
+public class Main {
+    public static void main(String[] args) {
+
+        for ( int contador  =5;contador >=0; contador = contador -1) {
+            System.out.println(contador);
+
+        }  System.out.println( "Acá terminó el ejercicio wachín");
+    }
+}
